27578f32893b40756453938fea12155857cf25d5
Baptiste Mesta <baptiste.mesta@gmail.com>
2017-03-10 11:16:14 +0100

    feat(scafold): akka hello world

--- src/main/scala/com/bmesta/engine/Engine.scala
+++ src/main/scala/com/bmesta/engine/Engine.scala
@@ -1,8 +1,22 @@
 package com.bmesta.engine
 
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props, Terminated}
+
 /**
   * @author Baptiste Mesta.
   */
 object Engine extends App {
-  System.out.println("hello")
+
+  val system = ActorSystem("Hello")
+  val a = system.actorOf(Props[HelloWorld], "helloWorld")
+  system.actorOf(Props(classOf[Terminator], a), "terminator")
+}
+
+class Terminator(ref: ActorRef) extends Actor with ActorLogging {
+  context watch ref
+  def receive = {
+    case Terminated(_) =>
+      log.info("{} has terminated, shutting down system", ref.path)
+      context.system.terminate()
+  }
 }
